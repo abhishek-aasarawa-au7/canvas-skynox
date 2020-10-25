7d0d9c9edb5fbca1300231c7c954502162158773
abhishek-aasarawa-au7 <aasarawa@gmail.com>
2020-10-25 11:59:21 +0530

    project setup

---
+++ src/client/build/precache-manifest.5356d4ff671c3f4cca486126eccabdbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080a43c942c75a8133184e25f8934713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595cca3c82a54f4fd3ac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "048bc08bdcaa9f00c4fa",
+    "url": "/static/js/2.877545d1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.877545d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ce00412ee148a30469",
+    "url": "/static/js/3.aa4eb9cf.chunk.js"
+  },
+  {
+    "revision": "595cca3c82a54f4fd3ac",
+    "url": "/static/js/main.57fefd23.chunk.js"
+  },
+  {
+    "revision": "34e8ab342c7b2a9bf7a7",
+    "url": "/static/js/runtime-main.26ab3f6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
